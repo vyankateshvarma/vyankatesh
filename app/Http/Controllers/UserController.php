8c9ec3e520bc464758af7ca24f7ef2976338c8c7
vyankatesh Varma <vyankatesh.varma@indipe.in>
2025-10-30 11:53:42 +0530

    model database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 class UserController
 {
-    function start(){
-        return DB::select('select * from users');
+    function getData(){
+        return User::all();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\DemoController;
-use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FormController;
-use App\Models\User;
-use App\Http\Middleware\agecheck;
 
-Route::get("users",[UserController::class,"start"]);
+// Your route definitions
+Route::get('/user', [UserController::class, 'getData']);
